1b610e9889dcab0420ccc9c0890cd0c28adc510c
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-07-24 15:35:21 +0430

    Complete the single and scss

--- index.php
+++ index.php
@@ -1,44 +1,42 @@
 <?php get_header(); ?>
 
-<main>
-    <div class="page-header"><h2><?php bloginfo('name'); ?></h2>
-        <p><?php bloginfo('description'); ?></p>
+    <div class="page-header"><h2><?php bloginfo( 'name' ); ?></h2>
+        <p><?php bloginfo( 'description' ); ?></p>
     </div>
 
     <div class="container">
         <div id="primary" class="content-area">
             <main id="main" class="site-main">
                 <div class="row">
-	                <?php
-	                if ( have_posts() ) :
+					<?php
+					if ( have_posts() ) :
 
-		                if ( is_home() && ! is_front_page() ) :
-			                ?>
+						if ( is_home() && ! is_front_page() ) :
+							?>
                             <header>
                                 <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
                             </header>
-		                <?php
-		                endif;
+						<?php
+						endif;
 
-		                /* Start the Loop */
-		                while ( have_posts() ) :
-			                the_post();
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
 
-			                /*
+							/*
 							 * Include the Post-Type-specific template for the content.
 							 * If you want to override this in a child theme, then include a file
 							 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 							 */
-			                get_template_part( 'template-parts/content', get_post_type() );
+							get_template_part( 'template-parts/content', get_post_type() );
 
-		                endwhile;
+						endwhile;
 
-	                endif;
-	                ?>
+					endif;
+					?>
                 </div>
             </main><!-- #main -->
         </div><!-- #primary -->
     </div>
-</main>
 
 <?php get_footer(); ?>
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -10,17 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php hs_post_thumbnail(); ?>
-
-    <div class="entry-meta">
-        <span><?php hs_the_category(); ?></span>
-    </div><!-- .entry-meta -->
-
-    <div class="entry-header">
-		<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
-    </div><!-- .entry-header -->
-
     <div class="entry-content">
-		<?php echo wp_trim_words( get_the_content(), 20 ); ?>
+		<?php the_content(); ?>
     </div><!-- .entry-content -->
 </article><!-- #post-<?php the_ID(); ?> -->
