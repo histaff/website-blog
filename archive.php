c9ab02ef4f0dbb41ca11dd3a702287421ca07fa2
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-07-27 19:19:47 +0430

    Update css

--- archive.php
+++ archive.php
@@ -1,53 +1,47 @@
-<?php
-/**
- * The template for displaying archive pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package HiStaff
- */
+<?php get_header(); ?>
 
-get_header();
-?>
+    <div class="page-header"><h2><?php bloginfo( 'name' ); ?></h2>
+        <p><?php bloginfo( 'description' ); ?></p>
+    </div>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+    <div class="container">
+        <div class="cats">
+            <span>Filter By Category :</span>
+            <ul><?php wp_list_categories('hide_empty=0&title_li='); ?></ul>
+        </div>
 
-		<?php if ( have_posts() ) : ?>
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main">
+                <div class="row">
+					<?php
+					if ( have_posts() ) :
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+						if ( is_home() && ! is_front_page() ) :
+							?>
+                            <header>
+                                <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                            </header>
+						<?php
+						endif;
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+							/*
+							 * Include the Post-Type-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', get_post_type() );
 
-			endwhile;
+						endwhile;
 
-			the_posts_navigation();
+					endif;
+					?>
+                </div>
+            </main><!-- #main -->
+        </div><!-- #primary -->
+    </div>
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
--- search.php
+++ search.php
@@ -1,55 +1,47 @@
-<?php
-/**
- * The template for displaying search results pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
- * @package HiStaff
- */
+<?php get_header(); ?>
 
-get_header();
-?>
+    <div class="page-header"><h2><?php bloginfo( 'name' ); ?></h2>
+        <p><?php bloginfo( 'description' ); ?></p>
+    </div>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+    <div class="container">
+        <div class="cats">
+            <span>Filter By Category :</span>
+            <ul><?php wp_list_categories('hide_empty=0&title_li='); ?></ul>
+        </div>
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main">
+                <div class="row">
 					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'histaff' ), '<span>' . get_search_query() . '</span>' );
+					if ( have_posts() ) :
+
+						if ( is_home() && ! is_front_page() ) :
+							?>
+                            <header>
+                                <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                            </header>
+						<?php
+						endif;
+
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
+
+							/*
+							 * Include the Post-Type-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', get_post_type() );
+
+						endwhile;
+
+					endif;
 					?>
-				</h1>
-			</header><!-- .page-header -->
+                </div>
+            </main><!-- #main -->
+        </div><!-- #primary -->
+    </div>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
