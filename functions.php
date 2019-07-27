2e55936a1cbf6669b2d3e6f907cde91150b0e604
Mostafa Soufi <mostafa.soufi@hotmail.com>
2019-07-27 19:31:47 +0430

    Added theme version

--- functions.php
+++ functions.php
@@ -116,7 +116,9 @@ add_action( 'widgets_init', __NAMESPACE__ . '\widgets_init' );
  * Enqueue scripts and styles.
  */
 function scripts() {
-	wp_enqueue_style( 'histaff-style', get_template_directory_uri() . '/assets/css/style.css' );
+	$theme = wp_get_theme();
+
+	wp_enqueue_style( 'histaff-style', get_template_directory_uri() . '/assets/css/style.css', false, $theme->Version );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
